3c7e4811d792c003d91f6fdc670eabd47fbeb520
olsentod <olsentod@gmail.com>
2020-09-28 14:55:26 -0600

    added stuff for DAO

--- sqlite3/employeeDAO1Sqlite.js
+++ sqlite3/employeeDAO1Sqlite.js
@@ -1,7 +1,92 @@
+const personDAO = require('./personDAO1Sqlite');
 
-const create = (db, obj) => {
-    db.run(`insert into persons values($id, $first, $middle, $last, $dob, $phone, $email, $street, $city, $state, $zip)`, obj);
-    console.log('Created');
+
+const create = async (db, employee) => {
+    return new Promise(async (resolve, reject) => {
+        const personsId = await personDAO.create(db, employee);
+
+        const employeeStatement = db.prepare("INSERT INTO employees VALUES (?, ?, ?, ?, ?, ?, ?)");
+        employeeStatement.run(
+            employee.id,
+            personsId,
+            employee.companyId,
+            employee.department,
+            employee.title,
+            employee.salary,
+            employee.managerId,
+        );
+
+        employeeStatement.finalize();
+    });
 }
 
+const read = (db, employeeId) => {
+    return new Promise((resolve, reject) => {
+        const employeeStatement = 'SELECT * from employees WHERE id = ?';   // Prepare the select statement
+        db.get(employeeStatement, [employeeId], async (err, row) => {
+            if (err) {
+                console.error('ERROR: ', err);
+                reject(err);
+            } else {
+                let person = await personDAO.read(db, row.personsId);       // Get the person infromation
+                resolve({...person, ...row});                               // Join employee info and person info and return
+            }
+        });
+    })
+}
+
+const update = (db, employee) => {
+    return new Promise(async (resolve, reject) => {
+        const personsId = await personDAO.update(db, employee);
+
+        const employeeStatement = db.prepare("UPDATE employees SET companyId = ?, department = ?, title = ?, salary = ?, managerId = ? WHERE id = ?");
+        employeeStatement.run(
+            employee.companyId,
+            employee.department,
+            employee.title,
+            employee.salary,
+            employee.managerId,
+            employee.id,
+        );
+        employeeStatement.finalize();
+
+        resolve(personsId);
+    });
+}
+
+const del = (db, employee) => {
+    return new Promise(async (resolve, reject) => {
+        await personDAO.del(db, employee.personsId);
+
+        const employeeStatement = db.prepare("DELETE FROM employees WHERE id = ?");
+        employeeStatement.run(
+            employee.id
+        );
+        employeeStatement.finalize();
+        resolve();
+    });
+}
+
+const list = (db) => {
+    return new Promise((resolve, reject) => {
+        const employeeStatement = 'SELECT * from employees';
+        db.all(employeeStatement, [], async (err, rows) => {
+            if (err) {
+                console.error('ERROR: ', err);
+                reject(err);
+            } else {
+                const employeeList = [];
+                for(const row of rows){
+                    const person = await personDAO.read(db, row.personsId);
+                    employeeList.push({...person, ...row});
+                }
+                resolve(employeeList);
+            }
+        });
+    })
+}
 exports.create = create;
+exports.read = read;
+exports.update = update;
+exports.del = del;
+exports.list = list;
--- sqlite3/main.js
+++ sqlite3/main.js
@@ -7,22 +7,32 @@ const db = new sqlite3.Database('./db/orm1', (err) => {
     }
 })
 
-const main = () => {
+const main = async () => {
     const employee = {
-        $id: '100',
-        $first: 'Roman',
-        $middle: 'V',
-        $last: 'Witting',
-        $dob: '07031997',
-        $phone: '4351231234',
-        $email: 'Horacio@yahoo.com',
-        $street: '80481 Betsy',
-        $city: 'East Darren',
-        $state: 'Kansas',
-        $zip: '69864',
-      }
-    employeeDAO.create(db,employee);
-
+        id: '10',
+        first: 'Tod',
+        middle: '',
+        last: 'Hehe',
+        dob: '07031997',
+        phone: '4351231234',
+        email: 'Horacio@yahoo.com',
+        street: '80481 Betsy',
+        city: 'East Darren',
+        state: 'Kansas',
+        zip: '69864',
+        // personsId: '398efa5b-6b8f-4c91-b0bc-14055a719258',
+        companyId: '619e9633-9d82-4f60-aaaf-39ec5a812294',
+        department: 'Coordinator',
+        title: 'Future Research Designer',
+        salary: 479770,
+        managerId: '4'
+    }
+    await employeeDAO.create(db, employee);
+    // await employeeDAO.update(db, employee);
+    // await employeeDAO.del(db, employee);
+    // const returnedEmp = await employeeDAO.read(db, newEmployee.id);
+    // console.log('found', returnedEmp);
+    // console.log('list', returnedEmps);
     db.close();
 }
 
