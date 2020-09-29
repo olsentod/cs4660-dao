86636adc3f0b83df10ffc3440067e91375c5a43f
Roman Meredith <romanmeredith@gmail.com>
2020-09-28 19:26:23 -0600

    added iterations in main

--- Contractor.js
+++ Contractor.js
@@ -3,6 +3,7 @@ const Nonemployee = require('./Nonemployee');
 class Contractor extends Nonemployee {
     constructor (id, first, middle, last, dob, phone, email, street, city, state, zip, company) {
         super(id, first, middle, last, dob, phone, email, street, city, state, zip, company);
+        this.type = 'contractor';
     }
 }
 
--- Vendor.js
+++ Vendor.js
@@ -3,6 +3,7 @@ const Nonemployee = require('./Nonemployee');
 class Vendor extends Nonemployee {
     constructor (id, first, middle, last, dob, phone, email, street, city, state, zip, company) {
         super(id, first, middle, last, dob, phone, email, street, city, state, zip, company);
+        this.type = 'vendor';
     }
 }
 
