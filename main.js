42404efa55ec6a49b99dfb34d12f6a07db7a7b4f
Roman Meredith <romanmeredith@gmail.com>
2020-09-28 21:14:59 -0600

    added arrays to database

--- main.js
+++ main.js
@@ -7,6 +7,9 @@ const Vendor = require('./Vendor');
 const VendorDAO = require('./sqlite3/vendorDAO1Sqlite');
 const CustomerDAO = require('./sqlite3/customerDAO1Sqlite');
 const ContractorDAO = require('./sqlite3/contractorDAO1Sqlite');
+const ManagerDAO = require('./sqlite3/managerDAO1Sqlite');
+const EmployeeDAO = require('./sqlite3/employeeDAO1Sqlite');
+const ExecutiveDAO = require('./sqlite3/executiveDAO1Sqlite');
 const sqlite3 = require('sqlite3').verbose();
 
 const db = new sqlite3.Database('./sqlite3/db/orm1', (err) => {
@@ -162,17 +165,29 @@ const main = () => {
     // console.log(contractors)
     // console.log(customers)
     // console.log(vendors)
-    // for(let vendor of vendors) {
-    //     VendorDAO.create(db, vendor)
-    // }
+    for(let executive of executives) {
+        EmployeeDAO.create(db, executive)
+    }
 
-    // for(let customer of customers) {
-    //     CustomerDAO.create(db, customer)
-    // }
+    for(let manager of managers) {
+        ManagerDAO.create(db, manager)
+    }
+
+    for(let employee of employees) {
+        EmployeeDAO.create(db, employee)
+    }
+
+    for(let vendor of vendors) {
+        VendorDAO.create(db, vendor)
+    }
+
+    for(let customer of customers) {
+        CustomerDAO.create(db, customer)
+    }
 
     for(let contractor of contractors) {
         ContractorDAO.create(db, contractor)
-    }
+    } 
 }
 
 main();
